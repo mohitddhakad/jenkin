0922c0d3b045dda59c0949c7f9dd76308c72376b
Mohit Dhakad <mohit12345150@gmail.com>
2026-02-12 09:31:03 +0530

    Add greeting message to App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,5 +2,9 @@ package com.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from kldfgjdig");
+System.out.println("hello to the world");
+
+
+
     }
 }
